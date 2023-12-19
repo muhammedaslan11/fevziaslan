b129b4e06b8a889b205657a41c64e13cab943fa2
Muhammed Aslan <aslanmuhammed2534@gmail.com>
2023-12-19 18:48:09 +0300

    ...

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,24 +1,33 @@
+import { TypeAnimation } from "react-type-animation";
+
 export default function HeroSection() {
   return (
-    <section id="heroSection" className="hero--section">
-      <div className="hero--section--content--box">
-        <div className="hero--section--content">
-          <p className="section--title">Hey, I'm John</p>
-          <h1 className="hero--section--title">
-            <span className="hero--section-title--color">Full Stack</span>{" "}
-            <br />
-            Developer
-          </h1>
-          <p className="hero--section-description">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            <br /> Dolorum, quas. Amet soluta assumenda cum?
-          </p>
+    <>
+      <div style={{ width: "100%", height: "73px", backgroundColor: "red" }} />
+      <section id="heroSection" className="hero--section">
+        <div className="hero--section--content--box">
+          <div className="hero--section--content">
+            <p className="section--title">
+              <TypeAnimation
+                sequence={["Merhaba, Ben Fevzi", 700, "", 700]}
+                repeat={Infinity}
+              />
+            </p>
+            <h1 className="hero--section--title">
+              <span className="hero--section-title--color">İnşaat</span> <br />
+              Mühendisi
+            </h1>
+            <p className="hero--section-description">
+              Sizlere projelerinizi Bize Sunun
+              <br /> Biz Onları Hayata Geçirelim...
+            </p>
+          </div>
+          <button className="btn btn-primary">İletişime Geç</button>
         </div>
-        <button className="btn btn-primary">Get In Touch</button>
-      </div>
-      <div className="hero--section--img">
-        <img src="./img/hero_img.png" alt="Hero Section" />
-      </div>
-    </section>
+        <div className="hero--section--img">
+          <img src="./img/fevz.png" alt="Hero Section" />
+        </div>
+      </section>
+    </>
   );
 }
--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -35,7 +35,7 @@ function Navbar() {
   return (
     <nav className={`navbar ${navActive ? "active" : ""}`}>
       <div>
-        <img src="./img/logo.svg" alt="Logoipsum" />
+        <img width={150} src="./img/logo2.png" alt="Logoipsum" />
       </div>
       <a
         className={`nav__hamburger ${navActive ? "active" : ""}`}
