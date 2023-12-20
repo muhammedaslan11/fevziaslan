af406b82f8ff13b9ef92e23fb92b94bf689bcc3e
Muhammed Aslan <aslanmuhammed2534@gmail.com>
2023-12-20 23:11:46 +0300

    deleted unneccessary files

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,20 +1,12 @@
-import AboutMe from "../AboutMe";
-import ContactMe from "../ContactMe";
 import Footer from "../Footer";
 import HeroSection from "../HeroSection";
-import MyPortfolio from "../MyPortfolio";
-import MySkills from "../MySkills";
-import Testimonial from "../Testimonials";
+import AboutMe from "../AboutMe";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      {/* <MySkills />
       <AboutMe />
-      <MyPortfolio />
-      <Testimonial />
-      <ContactMe /> */}
       <Footer />
     </>
   );
